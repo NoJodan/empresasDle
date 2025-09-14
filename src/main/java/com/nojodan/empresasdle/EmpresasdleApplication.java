ca21057be5cb6ded8d6724711f7c85d4b76d2fa9
Lawliet <edwardmario123@gmail.com>
2025-09-14 09:15:28 -0500

    [FEAT] Inicialización del proyecto

---
+++ src/main/java/com/nojodan/empresasdle/EmpresasdleApplication.java
@@ -0,0 +1,13 @@
+package com.nojodan.empresasdle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresasdleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresasdleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nojodan/empresasdle/EmpresasdleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nojodan.empresasdle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresasdleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
